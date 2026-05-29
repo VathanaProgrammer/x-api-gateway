f2633fb6722dfd4384f4aabf45a47f1925878b2b
Vathana Programmer <vathana@example.com>
2026-05-29 19:17:21 +0700

    Initial commit

---
+++ src/main/java/com/vyntra/gateway/VyntraApiGatwayApplication.java
@@ -0,0 +1,13 @@
+package com.vyntra.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyntraApiGatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VyntraApiGatwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Vyntra_ApiGatway/VyntraApiGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vyntra_ApiGatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VyntraApiGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
